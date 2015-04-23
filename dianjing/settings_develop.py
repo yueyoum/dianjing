ca1a12f6eb20605829b703a65b904313a7d74203
yueyoum <yueyoum@gmail.com>
2015-04-23 11:14:06 +0800

    add GameResponseMiddlwware

--- dianjing/settings_develop.py
+++ dianjing/settings_develop.py
@@ -51,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'dianjing.middleware.GameRequestMiddleware',
+    'dianjing.middleware.GameResponseMiddleware',
     'dianjing.middleware.GameExceptionMiddleware',
 )
 
