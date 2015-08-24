87fe9d11fa0c258d6c84a219671c8b609f578eb5
hikaly <said047@163.com>
2015-08-24 19:15:23 +0800

    test2

---
+++ apps/helper/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'apps.helper.app.ProjectConfig'
---
+++ apps/helper/app.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Author:         Wang Chao <yueyoum@gmail.com>
+Filename:       app
+Date Created:   2015-08-19 16:16
+Description:
+
+"""
+
+import os
+from django.apps import AppConfig
+
+class ProjectConfig(AppConfig):
+    name = 'apps.helper'
+
+
+    def ready(self):
+        from core.db import connect
+        connect()
+
+        from config import load_config
+        load_config()
+
+        import signals
+
+        if os.environ.get('UWSGI_RUNNING', '0') == '1':
+            import cronjob
