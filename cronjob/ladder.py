e0e4517f92060b89a9840ab021e2b520d825d96f
yueyoum <yueyoum@gmail.com>
2015-09-11 14:57:16 +0800

    Ladder Store buy times cron

--- cronjob/ladder.py
+++ cronjob/ladder.py
@@ -11,6 +11,7 @@ import traceback
 import uwsgidecorators
 
 from core.db import MongoDB
+from core.mongo import MongoLadder
 from cronjob.log import Logger
 
 from core.ladder import Ladder
@@ -24,6 +25,27 @@ def ladder_send_rank_reward(*args):
         servers = MongoDB.server_ids()
         for s in servers:
             Ladder.send_rank_reward(s)
+            logger.write("Server {0} finish".format(s))
+    except:
+        logger.error(traceback.format_exc())
+    else:
+        logger.write("Done")
+    finally:
+        logger.close()
+
+
+@uwsgidecorators.cron(0, 0, -1, -1, -1, target="spooler")
+def ladder_store_clean_buy_times(*args):
+    logger = Logger("ladder_store_clean_buy_times")
+    logger.write("Start")
+
+    try:
+        for s in MongoDB.server_ids():
+            MongoLadder.db(s).update_many(
+                {},
+                {'$set': {'buy_times': {}}}
+            )
+
             logger.write("Server {0} finish".format(s))
     except:
         logger.error(traceback.format_exc())
