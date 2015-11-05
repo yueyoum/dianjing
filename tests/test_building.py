7d03827ea13d6c90a549f008ddc486b58c94d504
hikaly <said047@163.com>
2015-11-05 20:00:42 +0800

    modify test_building.py

--- tests/test_building.py
+++ tests/test_building.py
@@ -21,8 +21,8 @@ from config import ConfigErrorMessage, ConfigBuilding
 def set_building_level(building_id, level):
     MongoBuilding.db(1).update_one(
         {'_id': 1},
-        {'$set': {'buildings.{0}.current_level'.format(building_id): level,
-                  'buildings.{0}.complete_time'.format(building_id): -1}},
+        {'$set': {'buildings.{0}.level'.format(building_id): level,
+                  'buildings.{0}.end_at'.format(building_id): 0}},
         upsert=True
     )
 
@@ -30,11 +30,23 @@ def set_building_level(building_id, level):
 def get_random_valid_building():
     while True:
         b = random.choice(ConfigBuilding.INSTANCES.values())
-        b_cfg = ConfigBuilding.get(b.id)
-        need_club_level = b_cfg.get_level(b.max_levels - 1).up_need_club_level
-        need_gold = b_cfg.get_level(b.max_levels - 1).up_need_gold
-        if need_gold > 0 and need_club_level >= b.max_levels - 1:
-            return b, need_club_level, need_gold
+        if get_need_club_lv(b.id, 2) & get_need_gold(b.id, 2) & get_up_need_time(b.id, 2):
+            return b
+
+
+def get_need_club_lv(building_id, level):
+    b_cfg = ConfigBuilding.get(building_id)
+    return b_cfg.get_level(level).up_need_club_level
+
+
+def get_need_gold(building_id, level):
+    b_cfg = ConfigBuilding.get(building_id)
+    return b_cfg.get_level(level).up_need_gold
+
+
+def get_up_need_time(building_id, level):
+    b_cfg = ConfigBuilding.get(building_id)
+    return b_cfg.get_level(level).up_need_minutes
 
 
 def get_not_exist_building():
@@ -44,19 +56,14 @@ def get_not_exist_building():
 
 
 class TestBuilding(object):
-    def reset(self):
-        MongoBuilding.db(1).delete_one({'_id': 1})
-
     def setUp(self):
-        self.reset()
         BuildingManager(1, 1)
 
     def tearDown(self):
-        self.reset()
         MongoBuilding.db(1).drop()
 
-    def test_getLevel(self):
-        b, need_club_level, need_gold = get_random_valid_building()
+    def test_get_level(self):
+        b = get_random_valid_building()
         set_building_level(b.id, b.max_levels)
         assert BuildingManager(1, 1).get_level(b.id) == b.max_levels
 
@@ -65,7 +72,6 @@ class TestBuilding(object):
 
     def test_level_up_building_not_exist(self):
         building_id = get_not_exist_building()
-
         try:
             BuildingManager(1, 1).level_up(building_id)
         except GameException as e:
@@ -74,7 +80,7 @@ class TestBuilding(object):
             raise Exception("can not be here!")
 
     def test_level_up_already_max_level(self):
-        b, need_club_level, need_gold = get_random_valid_building()
+        b = get_random_valid_building()
         set_building_level(b.id, b.max_levels)
         try:
             BuildingManager(1, 1).level_up(b.id)
@@ -83,28 +89,39 @@ class TestBuilding(object):
         else:
             raise Exception("can not be here!")
 
+    def test_level_up_building_upgrading(self):
+        b = get_random_valid_building()
+        try:
+            BuildingManager(1, 1).level_up(b.id)
+            BuildingManager(1, 1).level_up(b.id)
+        except GameException as e:
+            assert e.error_id == ConfigErrorMessage.get_error_id("BUILDING_IN_UPGRADING")
+        else:
+            raise Exception('error')
+
     def test_level_up_club_level_not_enough(self):
-        b, need_club_level, need_gold = get_random_valid_building()
+        b = get_random_valid_building()
         set_building_level(b.id, b.max_levels - 1)
 
         MongoCharacter.db(1).update_one(
             {'_id': 1},
-            {'$set': {'club.level': need_club_level - 1}}
+            {'$set': {'club.level': get_need_club_lv(b.id, b.max_levels-1) - 1}}
         )
         try:
             BuildingManager(1, 1).level_up(b.id)
         except GameException as e:
+            print e.error_id
             assert e.error_id == ConfigErrorMessage.get_error_id("CLUB_LEVEL_NOT_ENOUGH")
         else:
             raise Exception("can not be here!")
 
     def test_level_up_gold_not_enough(self):
-        b, need_club_level, need_gold = get_random_valid_building()
+        b = get_random_valid_building()
         set_building_level(b.id, b.max_levels - 1)
 
         MongoCharacter.db(1).update_one(
             {'_id': 1},
-            {'$set': {'club.level': need_club_level}}
+            {'$set': {'club.level': get_need_club_lv(b.id, b.max_levels-1)}}
         )
 
         try:
@@ -115,40 +132,39 @@ class TestBuilding(object):
             raise Exception("can not be here!")
 
     def test_level_up(self):
-        b, need_club_level, need_gold = get_random_valid_building()
+        b = get_random_valid_building()
         set_building_level(b.id, b.max_levels - 1)
 
         MongoCharacter.db(1).update_one(
             {'_id': 1},
             {'$set': {
-                'club.level': need_club_level,
-                'club.gold': need_gold
+                'club.level': get_need_club_lv(b.id, b.max_levels-1),
+                'club.gold': get_need_gold(b.id, b.max_levels -1),
             }}
         )
 
         BuildingManager(1, 1).level_up(b.id)
         data = MongoBuilding.db(1).find_one({'_id': 1}, {'buildings.{0}'.format(b.id): 1})
-        assert data['buildings'][str(b.id)]['complete_time'] != -1
+        assert data['buildings'][str(b.id)]['end_at'] > 0
         assert MongoCharacter.db(1).find_one({'_id': 1})['club']['gold'] == 0
 
-    def test_levelComfirm(self):
-        b, need_club_level, need_gold = get_random_valid_building()
-        set_building_level(b.id, b.max_levels - 1)
-
-        MongoCharacter.db(1).update_one(
-            {'_id': 1},
-            {'$set': {
-                'club.level': need_club_level,
-                'club.gold': need_gold
-            }}
-        )
-
+    def test_levelup_callback_not_end(self):
+        b = get_random_valid_building()
         BuildingManager(1, 1).level_up(b.id)
-        MongoBuilding.db(1).update_one(
-            {'_id': 1},
-            {'$set': {'buildings.{0}.complete_time'.format(b.id): 0}}
-        )
-        BuildingManager(1, 1).levelup_confirm()
+        ret = BuildingManager(1, 1).levelup_callback(b.id)
+        assert ret > 0
+
+    def test_levelup_callback(self):
+        b = get_random_valid_building()
+        set_building_level(b.id, 1)
+        BuildingManager(1, 1).level_up(b.id)
+
+        t = get_up_need_time(b.id, 1)
+        print t
+        import time
+        time.sleep(t * 60)
+        BuildingManager(1, 1).levelup_callback(b.id)
+
         data = MongoBuilding.db(1).find_one({'_id': 1}, {'buildings.{0}'.format(b.id): 1})
-        assert data['buildings'][str(b.id)]['complete_time'] == -1
-        assert BuildingManager(1, 1).get_level(b.id) == b.max_levels
+        assert data['buildings'][str(b.id)]['level'] == 2
+
