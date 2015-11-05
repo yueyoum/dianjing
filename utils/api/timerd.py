1b3951d82c2a8820021a6f6506f2419055f17cfd
yueyoum <yueyoum@gmail.com>
2015-11-05 14:52:32 +0800

    make Timerd lazy init

--- utils/api/timerd.py
+++ utils/api/timerd.py
@@ -8,21 +8,49 @@ Description:
 """
 import json
 from urlparse import urljoin
-from django.conf import settings
 
 import requests
 
+
+def _setup(func):
+    def deco(cls, *args, **kwargs):
+        """
+
+        :type cls: Timerd
+        """
+        if not cls._SETUP:
+            cls.setup()
+
+        return func(cls, *args, **kwargs)
+
+    return deco
+
+
 class Timerd(object):
-    URL_REGISTER = urljoin(settings.TIMERD_URL, '/register/')
-    URL_QUERY = urljoin(settings.TIMERD_URL, '/query/')
-    URL_CANCEL = urljoin(settings.TIMERD_URL, '/cancel/')
+    _SETUP = False
+
+    URL_REGISTER = ''
+    URL_QUERY = ''
+    URL_CANCEL = ''
 
     @classmethod
+    def setup(cls):
+        from django.conf import settings
+
+        cls.URL_REGISTER = urljoin(settings.TIMERD_URL, '/register/')
+        cls.URL_QUERY = urljoin(settings.TIMERD_URL, '/query/')
+        cls.URL_CANCEL = urljoin(settings.TIMERD_URL, '/cancel/')
+
+        cls._SETUP = True
+
+    @classmethod
+    @_setup
     def register(cls, end, callback_path, callback_data):
         """
 
         :rtype : str
         """
+        from django.conf import settings
 
         callback_url = urljoin(settings.SERVER_HOST, callback_path)
         data = {
@@ -35,6 +63,7 @@ class Timerd(object):
         return req.json()['key']
 
     @classmethod
+    @_setup
     def query(cls, key):
         """
 
@@ -48,6 +77,7 @@ class Timerd(object):
         return req.json()['ttl']
 
     @classmethod
+    @_setup
     def cancel(cls, key):
         data = {
             'key': key
@@ -56,4 +86,3 @@ class Timerd(object):
         req = requests.post(cls.URL_CANCEL, data=data)
         assert req.ok is True
         return None
-
