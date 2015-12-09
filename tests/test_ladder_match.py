329e774b65b7ddf1aed86297ba4799005a7118ed
hikaly <said047@163.com>
2015-12-09 09:47:26 +0800

    modify

--- tests/test_ladder_match.py
+++ tests/test_ladder_match.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Author:         Ouyang_Yibei <said047@163.com>
-Filename:       test_ladder-match
-Date Created:   2015-12-07 10:25
+Filename:       test_ladder_match
+Date Created:   2015-12-07 10:33
 Description:
 
 """
+
+from core.ladder.match import LadderMatch, LadderNPCClub
+
+
+class TestLadderMatch(object):
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+
