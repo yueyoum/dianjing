1c2266423c62f29e1c5a93bdd7a77d6f4bb59521
yueyoum <yueyoum@gmail.com>
2015-12-11 11:12:35 +0800

    update to Django 1.9

--- views/urls_api.py
+++ views/urls_api.py
@@ -9,10 +9,12 @@ Description:
 
 from django.conf.urls import url
 
+import views.api.timerd
+
 urlpatterns = [
-    url(r'^timerd/building/$', 'views.api.timerd.building_levelup_callback'),
-    url(r'^timerd/training/exp/$', 'views.api.timerd.training_exp_callback'),
-    url(r'^timerd/training/property/$', 'views.api.timerd.training_property_callback'),
-    url(r'^timerd/training/broadcast/$', 'views.api.timerd.training_broadcast_callback'),
-    url(r'^timerd/skill/$', 'views.api.timerd.skill_upgrade_callback'),
+    url(r'^timerd/building/$', views.api.timerd.building_levelup_callback),
+    url(r'^timerd/training/exp/$', views.api.timerd.training_exp_callback),
+    url(r'^timerd/training/property/$', views.api.timerd.training_property_callback),
+    url(r'^timerd/training/broadcast/$', views.api.timerd.training_broadcast_callback),
+    url(r'^timerd/skill/$', views.api.timerd.skill_upgrade_callback),
 ]
