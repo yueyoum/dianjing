715534637426aeff6df0f83414a489f0dbfe05c1
yueyoum <yueyoum@gmail.com>
2016-06-14 10:21:00 +0800

    task daily reset

--- cronjob/task.py
+++ cronjob/task.py
@@ -12,6 +12,7 @@ import traceback
 import uwsgidecorators
 from apps.server.models import Server
 from core.task import RandomEvent
+from core.mongo import MongoTaskDaily
 from cronjob.log import Logger
 
 
@@ -23,6 +24,24 @@ def reset_random_event(*args):
     try:
         for sid in Server.opened_server_ids():
             RandomEvent.cronjob(sid)
+
+            logger.write("Server {0} Finish".format(sid))
+    except:
+        logger.error(traceback.format_exc())
+    else:
+        logger.write("Done")
+    finally:
+        logger.close()
+
+
+@uwsgidecorators.cron(0, 0, -1, -1, -1, target="spooler")
+def reset_task_daily(*args):
+    logger = Logger("reset_task_daily")
+    logger.write("Start")
+
+    try:
+        for sid in Server.opened_server_ids():
+            MongoTaskDaily.db(sid).drop()
 
             logger.write("Server {0} Finish".format(sid))
     except:
