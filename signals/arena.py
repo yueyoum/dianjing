00c3bbd66348baea4c425eb5238bd5aa8ac9c826
yueyoum <yueyoum@gmail.com>
2016-07-06 14:49:19 +0800

    fix

--- signals/arena.py
+++ signals/arena.py
@@ -13,7 +13,7 @@ from core.signals import arena_match_signal
 from core.system import BroadCast
 
 @receiver(arena_match_signal, dispatch_uid='signals.arena.match_handler')
-def match_handler(server_id, char_id, target_id, my_rank, rival_rank, win, **kwargs):
+def match_handler(server_id, char_id, target_id, my_rank, target_rank, win, **kwargs):
     if win:
         b = BroadCast(server_id, char_id)
-        b.cast_arena_match_notify(target_id, rival_rank)
+        b.cast_arena_match_notify(target_id, target_rank)
